2207fa45a74415f91254777ec8996789ad327f9a
Kaushal-Danani <Kaushal.Danani@zeuslearning.com>
2025-06-05 13:42:01 +0530

    Changes in Task-1 and Task-2 completed

--- Task-1/script.js
+++ Task-1/script.js
@@ -8,18 +8,24 @@ function validateForm(event)
 
     if (name == '') {
         alert('Please enter your name.');
+        document.getElementById('name').focus();
         return false;
     }
 
     if (comments == '') {
         alert('Please enter your comments.');
+        document.getElementById('comments').focus();
         return false;
     }
 
     if (!maleChecked.checked && !femaleChecked.checked) {
         alert('Please select your gender.');
+        document.getElementById('gender').style.border = "1px solid red";
         return false;
     }
     
+    alert('Form successfully submitted')
+    document.getElementById('gender').style.border = "none";
+    document.getElementById('myform').reset();
     return true;
 }
